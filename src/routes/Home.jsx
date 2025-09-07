dbe947284b4c1d869228556e908ffe3672534df6
Mico Irvin De Mesa <104616752+micoirvin@users.noreply.github.com>
2025-09-07 13:46:18 +0800

    Tested  init one-26 on drive

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,15 +4,27 @@ import Header from '../components/Header';
 import useUser from '../hooks/useUser';
 import useLoginTransition from '../hooks/useLoginTransition';
 import { Navigate } from 'react-router-dom';
+import urlToBackend from '../utils/urlToBackend';
 
 export default function Home() {
   const { accessToken, user, setUser } = useUser();
+
+  const test = () => {
+    fetch(urlToBackend('/api/find-one-26'), {
+      credentials: 'include',
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((error) => console.error(error));
+  };
 
   return (
     <main>
       LOGGED IN
       <Header />
       {/* <Gallery /> */}
+      <button onClick={test}>TEST</button>
     </main>
   );
 }

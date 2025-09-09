3bdee719c9c425ef72014ca33afe646b8e137edf
Mico Irvin De Mesa <104616752+micoirvin@users.noreply.github.com>
2025-09-09 08:38:31 +0800

    remove vars.js

--- src/vars.js
+++
@@ -1,8 +0,0 @@
-export default function vars() {
-  return {
-    ENVIRONMENT: 'DEV',
-    DEV_BACKEND_URL: 'https://localhost:4000/',
-    G_API_KEY: 'willbedeleted',
-    G_SCOPES: 'https://www.googleapis.com/auth/drive',
-  };
-}

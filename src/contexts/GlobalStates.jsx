45185ad20e51dba80663862d5cb86837cd5eec93
Mico Irvin De Mesa <104616752+micoirvin@users.noreply.github.com>
2025-09-09 08:29:15 +0800

    note for deletion of sensitive info

--- src/contexts/GlobalStates.jsx
+++ src/contexts/GlobalStates.jsx
@@ -2,8 +2,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import UserProvider from './UserContext';
 
 export default function GlobalStates({ children }) {
-  const CLIENT_ID =
-    '897745564559-kqcinnl857j6ptsj6ep8ll3m4n56a7oj.apps.googleusercontent.com';
+  const CLIENT_ID = 'willbedeleted';
   return (
     <GoogleOAuthProvider clientId={CLIENT_ID}>
       <UserProvider>{children}</UserProvider>
--- src/vars.js
+++ src/vars.js
@@ -2,7 +2,7 @@ export default function vars() {
   return {
     ENVIRONMENT: 'DEV',
     DEV_BACKEND_URL: 'https://localhost:4000/',
-    G_API_KEY: 'AIzaSyDnGbUxttb2LZNOfCc0OEVcBOu4J8PWHXY',
+    G_API_KEY: 'willbedeleted',
     G_SCOPES: 'https://www.googleapis.com/auth/drive',
   };
 }

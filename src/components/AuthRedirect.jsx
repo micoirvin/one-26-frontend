7cdabb286d70449220e24102383e4f7f48eb8403
Mico Irvin De Mesa <miydemesa@gmail.com>
2026-08-28 17:37:06 +0800

    changed initial load message

--- src/components/AuthRedirect.jsx
+++ src/components/AuthRedirect.jsx
@@ -14,7 +14,9 @@ export default function AuthRedirect({ children }) {
     return (
       <main>
         <section className="container py-8">
-          <div>Loading...</div>
+          <div>
+            Please wait... Our free service may take time for the initial load.
+          </div>
         </section>
       </main>
     );
--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -17,7 +17,9 @@ export default function LoginPage() {
         {user ? (
           <Navigate to="/" />
         ) : isLoading ? (
-          <div>Loading...</div>
+          <div>
+            Please wait... Our free service may take time for the initial load.
+          </div>
         ) : error ? (
           <div>An error occured. Try reloading</div>
         ) : (
